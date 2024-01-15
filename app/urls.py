8ecdc4ce4e47aefcec8802d52b025a2b64fe2b6c
Moshik Ziyoni <111297694+MoshikZiyoni@users.noreply.github.com>
2024-01-15 18:26:55 +0200

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ app_name = 'app'
 
 urlpatterns = [
     path("ai-bot", views.ai_bot, name= "ai_bot"),
+    path("chat-with-ai", views.chat_with_ai, name= "chat_with_ai"),
     path("night-life", views.night_life, name= "night_life"),
     
 ]
